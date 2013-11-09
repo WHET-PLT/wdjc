ec567ded069bb2f0626834fbe1e18bbb90c2cb87
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-09 13:33:23 -0500

    comments and updates java note/chord/track

--- java/Track.java
+++ java/Track.java
@@ -13,14 +13,15 @@ public class Track {
 	public ArrayList<Chord> track;
 	
 	/**
-	 * 
+	 * Empty track constructor
 	 */
 	public Track() {
 		track = new ArrayList<Chord>();
 	}
 	
 	/**
-	 * 
+	 * Chord elevation Track constructor.
+	 * Builds a single-chord track from a chord
 	 * @param track
 	 */
 	public Track(Chord other) {
@@ -29,16 +30,17 @@ public class Track {
 	}
 	
 	/**
-	 * 
+	 * Track copy-constructor
 	 * @param track
 	 */
 	public Track(Track other) {
 		track = new ArrayList<Chord>();
+		//copy-constructor so pass-by-value of chord references
 		track.addAll(other.getTrack());
 	}
 	
 	/**
-	 * 
+	 * Get the actual arraylist
 	 * @return
 	 */
 	public ArrayList<Chord> getTrack() {
@@ -60,34 +62,48 @@ public class Track {
 	public int length() {
 		return track.size();
 	}
-	
+
 	/**
-	 * 
-	 * @param chord
-	 */
-	public void addChord(Chord chord) {
-		track.add(chord);
-	}
-	
-	/**
-	 * 
+	 * serial add tracks
 	 * @param other
 	 */
 	public Track serialAdd(Track other) {
+		//immutable.
 		Track tmp = new Track(this); 
+		//addAll is arraylist concatenation
 		tmp.getTrack().addAll(other.getTrack());;
+		return tmp;
+	}
+	
+	/**
+	 * add a chord to a track. "elevation" is handled
+	 * @param other
+	 */
+	public Track serialAdd(Chord other) {
+		Track tmp = new Track(this); 
+		tmp.getTrack().add(other);;
 		return tmp;
 	}
 
 	/**
-	 * 
+	 * parallel add chord to track
+	 * @param track
+	 */
+	public void parallelAdd(Chord other) {
+		Track tmp = new Track(this);
+		tmp.getChord(0).parallelAdd(other);
+	}
+	
+	/**
+	 * parallel add track to track
 	 * @param track
 	 */
 	public void parallelAdd(Track other) {
 		Track tmp = new Track(this);
+		//go through the length of the shorter track
 		for(int i = 0; i < (this.length() < other.length() ? this.length() : other.length()); i++) {
+			//par-add the second operand track to the tmp (left operand copy) track element at the same place
 			tmp.getChord(i).parallelAdd(other.getChord(i));
 		}
 	}
-	
 }
