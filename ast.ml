fdbfb1ac6e02a8cf62ebc2b328d30ad35c81be9a
Thomas <tee2103@columbia.edu>
2013-11-13 16:39:26 -0500

    modifies ast. gets precedence errors.

--- ast.ml
+++ ast.ml
@@ -69,7 +69,7 @@ let rec string_of_expr = function
   | Assign(v, e) -> v ^ " = " ^ string_of_expr e
   | Call(f, el) ->
       f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
-  | Array 
+(*| Array*) 
 
 
 (*pretty print for stmts*)
@@ -86,7 +86,7 @@ let rec string_of_stmt = function
       "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
       string_of_expr e3  ^ ") " ^ string_of_stmt s
   (*| While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s*)
-  | Loop
+ (*| Loop*)
 
 let string_of_vdecl id = "int " ^ id ^ ";\n"
 
