2eb6f0b45deb4af413e3181a028b9a56016b91a5
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-15 19:08:59 -0500

    fixes global bytecode something

--- wdjc.ml
+++ wdjc.ml
@@ -3,7 +3,9 @@ type action = Ast | Compile | Bytecode
 let _ =
   let action = if Array.length Sys.argv > 1 then
     List.assoc Sys.argv.(1) [ ("-a", Ast);
-    		 ("-c", Compile) ]
+            ("-i", Interpret);
+            ("-b", Bytecode);
+            ("-c", Compile) ]
   else Compile in
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
