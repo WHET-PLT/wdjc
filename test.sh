267c751675314ca2585aeca36246ea8b6bd49e8c
Thomas <tee2103@columbia.edu>
2013-11-16 12:43:14 -0500

    add temporary test script. must go in wdjc dir

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Temporary test script, ast only
+
+WDJC="./wdjc"
+
+# Set time limit for all operations
+ulimit -t 30
+
+path= 'pwd'
+echo $path
+
+for file in ./tests/*.dj
+do
+	echo $file
+	echo ------------------------
+	$WDJC -a < $file
+	echo
+done
