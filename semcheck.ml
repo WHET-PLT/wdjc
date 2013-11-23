bc715ec4586337f74d4dfa457caff8f6f3ec0560
Thomas <tee2103@columbia.edu>
2013-11-23 15:43:18 -0500

    modifies semcheck

--- semcheck.ml
+++ semcheck.ml
@@ -3,9 +3,9 @@ open Ast
 module StringMap = Map.Make(String)
 
 type env = {
-	functions:	string list Stringmap.t;
-	globals:	string Stringmap.t;
-	locals:		string Stringmap.t;
+	functions:	string list StringMap.t;
+	globals:	string StringMap.t;
+	locals:		string StringMap.t;
 }
 
 let string_of_vartype = function
@@ -15,23 +15,26 @@ let string_of_vartype = function
    | Track -> "track"
 
 let variable vname env = 
-	try Stringmap.find vname env.locals
-	with Not_found -> try Stringmap.find vname env.globals
+	try StringMap.find vname env.locals
+	with Not_found -> try StringMap.find vname env.globals
 	with Not_found -> raise (Failure ("undeclared variable " ^ vname))
 
 let function fname env =
-	try Stringmap.find fname env.functions
+	try StringMap.find fname env.functions
 	with Not_found -> raise (Failure ("undeclared variable " ^ fname))
 
 let add_local var_type name env =
-	if Stringmap.mem name env.locals then Stringmap.empty
-	else Stringmap.add name (string_of_vartype vtype) env.locals
+	if StringMap.mem name env.locals then StringMap.empty
+	else StringMap.add name (string_of_vartype vtype) env.locals
 
 let add_global var_type name env =
-	if Stringmap.mem name env.globals then Stringmap.empty
-	else Stringmap.add name (string_of_vartype vtype) env.globals
+	if StringMap.mem name env.globals then StringMap.empty
+	else StringMap.add name (string_of_vartype vtype) env.globals
 
 let get_type = function
 	var -> string_of_vartype var.vartype
 
 let add_function fname return formals env =
+	if StringMap.mem name env.functions then StringMap.empty
+	else let fmls = List.map get_type formals in
+	StringMap.add name (string_of_vartype (return) :: fmls) env.functions
