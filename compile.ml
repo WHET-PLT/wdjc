396e79711c9bd335d7deaa04e60f08d52e2da3ed
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-25 03:19:27 -0500

    compile.ml function to string filewriting

--- compile.ml
+++ compile.ml
@@ -1,5 +1,12 @@
 open Sast
 
 (* need ast to java main body string *)
-(* need body string to file *)
+(* need body string to file ('java') *)
 
+let function_string ftype fname formals locals body =
+  let jtype = ftype_string ftype
+  and jname = fname_string fname
+  and jformals = vdecllist_string formals
+  and jlocals = vdecllist_string locals
+  and jbody = stmtlist_string body in
+  fprintf java "public %s %s(%s) { %s %s }" jtype jname jformals jlocals jbody; 
