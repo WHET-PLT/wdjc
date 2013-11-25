7b4f3e8533900ca2037c74cadef3967c5107eeca
elemonier <emily.lemonier@gmail.com>
2013-11-24 19:40:48 -0500

    Commented and clarified semcheck.

--- semcheck.ml
+++ semcheck.ml
@@ -1,4 +1,21 @@
+(* 
+
+	semcheck.ml
+
+	- semantically checks the input file
+		- checks for type errors
+		- undefined variable & function errors
+	-converts from ast to sast
+
+*)
 open Ast
+open Sast
+
+(*NOTE:
+	map.find: returns the value associated with a key
+	map.mem: checks if value exists for a given key
+*)
+
 
 module StringMap = Map.Make(String)
 
@@ -8,42 +25,21 @@ type env = {
 	locals:		string StringMap.t;
 }
 
+
 let string_of_vartype = function
    Int -> "int"
    | Note -> "note"
    | Chord -> "chord"
    | Track -> "track"
 
-(*map.find: returns the value associated with a key*)
-(**)
-let get_variable vname env = 
-	try StringMap.find vname env.locals
-	with Not_found -> try StringMap.find vname env.globals
-	with Not_found -> raise (Failure ("undeclared variable " ^ vname))
 
-let get_function fname env =
-	try StringMap.find fname env.functions
-	with Not_found -> raise (Failure ("undeclared variable " ^ fname))
-
-(*map.mem: checks if value exists for a given key*)
-(*adds to map a local var if not found*)
-let add_local var_type name env =
-	if StringMap.mem name env.locals then StringMap.empty
-	else StringMap.add name (string_of_vartype vtype) env.locals
-
-(*adds to map a global var if not found*)
-let add_global var_type name env =
-	if StringMap.mem name env.globals then StringMap.empty
-	else StringMap.add name (string_of_vartype vtype) env.globals
-
-(*adds to map a function, formals, and return type if not found*)
-let add_function fname return formals env =
-	if StringMap.mem name env.functions then StringMap.empty
-	else let fmls = List.map get_type formals in
-	StringMap.add name (string_of_vartype (return) :: fmls) env.functions
 
 let get_type = function
 	var -> string_of_vartype var.vartype
+
+(*get type for variables *)
+
+(*get type for expressions*)
 
 (*gets sast types*)
 let ast_to_sast_type = function
@@ -52,4 +48,106 @@ let ast_to_sast_type = function
    | Ast.Chord -> Sast.Chord
    | Ast.Track -> Sast.Track
    | _ -> raise (Failure ("Mismatch type"))
+
+
+
+(* HELPFUL FUNCTIONS TO GET AND ADD VARIABLES (GLOBAL & LOCAL), FUNCIONS TO ENVIRONMENT *)
+
+(* 
+	get_variable vname env
+		vname - variable name
+		env - environment stringmap
+	Looks to find variable name in env's local list.
+	If it doesn't find it, it checks the env's global list.
+	If not found, raises error.
+*)
+let get_variable vname env = 
+	try StringMap.find vname env.locals
+	with Not_found -> try StringMap.find vname env.globals
+	with Not_found -> raise (Failure ("undeclared variable " ^ vname))
+
+
+(* 
+	get_function vname env
+		vname - function name
+		env - environment stringmap
+	Looks to find function name in env's function list.
+	If not found, raises error.
+*)
+let get_function fname env =
+	try StringMap.find fname env.functions
+	with Not_found -> raise (Failure ("undeclared variable " ^ fname))
+
+
+(*
+	add_local var_type name env
+		var_type - variable type
+		name - variable name
+		env - environment stringmap
+	Checks to see if the name is in env's local list.
+	If it doesn't contain it, it adds it to the env's local list.
+*)
+let add_local var_type name env =
+	if StringMap.mem name env.locals then StringMap.empty
+	else StringMap.add name (string_of_vartype vtype) env.locals
+
+(*
+	add_global var_type name env
+		var_type - variable type
+		name - gariable name
+		env - environment stringmap
+
+	Checks to see if the name is in the env's global list.
+	If it dlesn't contain it, it adds it to the env's global list.
+*)
+let add_global var_type name env =
+	if StringMap.mem name env.globals then StringMap.empty
+	else StringMap.add name (string_of_vartype vtype) env.globals
+
+(*
+	CONFUSED ON THE GET_TYPE 
+	add_function fname return formals env
+		fname - function name
+		return - return type
+		formals - formal arguments
+		env - environment stringmap
+
+	Checks to see if the fname is in env's function list
+	if not- it gets the types of the formals, adds:
+		name, vartype of return, formals to environemt's function
+*)
+let add_function fname return formals env =
+	if StringMap.mem name env.functions then StringMap.empty
+	else let fmls = List.map get_type formals in
+	StringMap.add name (string_of_vartype (return) :: fmls) env.funct`ions
+
+
+
+(* SEMANTIC CHECKING FUNCTIONS *)
+
+(* check binop operands *)
+(* check statement *)
+(* check statement list *)
+(* check formal list *)
+(* check function arguments *)
+(* check function *)
+(* check function list *)
+(* sem check global *)
+(* sem check global list *)
+(* semantically check program *)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
