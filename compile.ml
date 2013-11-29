f235062604f66a3f3517292847fa4c4cf55fed3e
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-29 10:58:30 -0500

    updates template from sast

--- compile.ml
+++ compile.ml
@@ -6,7 +6,22 @@ open Sast
 let function_string ftype fname formals locals body =
   let jtype = ftype_string ftype
   and jname = fname_string fname
-  and jformals = vdecllist_string formals
+  and jformals = paramlist_string formals
   and jlocals = vdecllist_string locals
   and jbody = stmtlist_string body in
   fprintf java "public %s %s(%s) { %s %s }" jtype jname jformals jlocals jbody; 
+
+
+let ftype_string ftype =
+  
+let fname_string fname =
+  
+let paramlist_string formals =
+
+let vdecllist_string locals = 
+  
+let stmtlist_string body =
+  
+(* expressions *)
+(* statements *)
+(* modifs and note attrs and ops? *)
