898f3fe03233d8ee0bc8548460f9830e3c501175
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-29 12:23:33 -0500

    adds type print

--- compile.ml
+++ compile.ml
@@ -5,16 +5,21 @@ open Sast
 
 let function_string ftype fname formals locals body =
   let jtype = ftype_string ftype
-  and jname = fname_string fname
+  and jname = fname
   and jformals = paramlist_string formals
   and jlocals = vdecllist_string locals
   and jbody = stmtlist_string body in
   fprintf java "public %s %s(%s) { %s %s }" jtype jname jformals jlocals jbody; 
 
-
-let ftype_string ftype =
-  
-let fname_string fname =
+(* need to figure out what the java library constructs are
+   that could complicate this *)
+let ftype_string ftype = 
+  match ftype with
+    Int -> "int"
+  | Note -> "note"
+  | Chord -> "chord"
+  | Track -> "track"
+  | Rest -> "rest"
   
 let paramlist_string formals =
 
