ecc4018ef8258f6051106803e7b8787ec5ff19dd
William Falk-Wallace <wfalkwallace@gmail.com>
2013-12-03 10:25:42 -0500

    fixes mergerz junk

--- wdjc.ml
+++ wdjc.ml
@@ -10,17 +10,10 @@ let _ =
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
   match action with
-<<<<<<< HEAD
       Ast -> let listing = Ast.string_of_program program
           in print_string listing
     (* | Java -> let listing = Compile.program_string (Semcheck.translate program)
           in print_endline listing *)
     | Java -> let listing = Compile.program_string program
               in print_endline listing
-=======
-  	  Ast -> let listing = Ast.string_of_program program
-          in print_string listing
-    | Java -> let listing = Compile.program_string (Semcheck.translate program)
-          in print_endline listing
->>>>>>> b90d94a4cb62bf1da306c17f64e7db8565741807
     | Compile -> Execute.javacompile (Compile.program_string (Semcheck.translate program))
